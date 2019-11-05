5e06ab6bec5bc97740c1c90ba81640b40d59abc0
karanshah197 <karanshah197@gmail.com>
2019-11-05 18:54:01 -0500

    Task: Added travis ci yml and setup python file

---
+++ seup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Statistical Calculator',
+    version='0.0.1',
+    description='Its a calculator with statistical functions',
+    author='Karan Shah ans Yesha Majmudar',
+    author_email='kvs32@njit.edu, yum2@njit.edu',
+    url='',
+    packages=find_packages(),
+)
