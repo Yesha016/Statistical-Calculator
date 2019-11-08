c8acfdba4b0f9e6917f734104bed4edf13c20043
Yesha016 <yum2@njit.edu>
2019-11-08 12:18:23 -0500

    Fix: Added Addition method

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -8,14 +8,7 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        # a = input("Enter value for a: ")
-        # b = input("Enter value for b: ")
-        try:
-            if a is None:
-                raise ValueError
-            if b is None:
-                raise ValueError
-            self.result = addition(int(a), int(b))
-            return self.result
-        except ValueError:
-            print("Please specify value a or b")
+        self.result = addition(a, b)
+        return self.result
+
+
