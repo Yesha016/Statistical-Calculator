a5514eee6aabe765655fe13ecd4d3d91d155c8d0
karanshah197 <karanshah197@gmail.com>
2019-11-08 16:56:07 -0500

    Fix: fixed the round off to Calculator

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -2,6 +2,7 @@ import unittest
 
 from Calculator.Calculator import Calculator
 from CsvReader.CsvReader import CsvReader
+from StaticMethods.roundOff import roundOff
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,26 +14,26 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_add_method_calculator(self):
-        result = 4
+        result = roundOff(4)
         self.assertEqual(self.calculator.add(2, 2), result)
         self.assertEqual(self.calculator.result, result)
 
     def test_add_method_calculatorCSV(self):
         test_data = CsvReader('Data/Addition.csv').data
         for row in test_data:
-            result = float(row['Result'])
+            result = roundOff(float(row['Result']))
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_subtract_method_calculator(self):
-        result = 0
+        result = roundOff(0)
         self.assertEqual(self.calculator.subtract(2, 2), result)
         self.assertEqual(self.calculator.result, result)
 
     def test_subtract_method_calculatorCSV(self):
         test_data = CsvReader('Data/Subtraction.csv').data
         for row in test_data:
-            result = float(row['Result'])
+            result = roundOff(float(row['Result']))
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
@@ -44,43 +45,43 @@ class MyTestCase(unittest.TestCase):
     def test_multiply_method_calculatorCSV(self):
         test_data = CsvReader('Data/Multiplication.csv').data
         for row in test_data:
-            result = float(row['Result'])
+            result = roundOff(float(row['Result']))
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_divide_method_calculator(self):
-        result = 2
+        result = roundOff(2)
         self.assertEqual(self.calculator.divide(4, 8), result)
         self.assertEqual(self.calculator.result, result)
 
     def test_divide_method_calculatorCSV(self):
         test_data = CsvReader('Data/Division.csv').data
         for row in test_data:
-            result = float(row['Result'])
+            result = roundOff((row['Result']))
             self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_square_method_calculator(self):
-        result = 16
+        result = roundOff(16)
         self.assertEqual(self.calculator.square(4), result)
         self.assertEqual(self.calculator.result, result)
 
     def test_square_method_calculatorCSV(self):
         test_data = CsvReader('Data/Square.csv').data
         for row in test_data:
-            result = float(row['Result'])
+            result = roundOff(float(row['Result']))
             self.assertEqual(self.calculator.square(row['Value 1']), result)
             self.assertEqual(self.calculator.result, result)
 
     def test_sqrt_method_calculator(self):
-        result = 2
+        result = roundOff(2)
         self.assertEqual(self.calculator.squareroot(4), result)
         self.assertEqual(self.calculator.result, result)
 
     def test_sqrt_method_calculatorCSV(self):
         test_data = CsvReader('Data/Square Root.csv').data
         for row in test_data:
-            result = float(row['Result'])
+            result = roundOff(float(row['Result']))
             self.assertAlmostEqual(self.calculator.squareroot(row['Value 1']), result)
             self.assertAlmostEqual(self.calculator.result, result)
 
