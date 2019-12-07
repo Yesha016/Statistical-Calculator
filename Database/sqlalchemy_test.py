f1a5db33a10a1531054e85335c19cba7aa7bc02d
Yesha016 <yum2@njit.edu>
2019-12-07 17:50:56 -0500

    Feature: Created customer table

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -5,6 +7,21 @@ engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+Base = declarative_base()
+
+class Customer(Base):
+    __tablename__ = 'Customer'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    username = Column(String)
+    email = Column(String)
+    address = Column(String)
+    town = Column(String)
+
+Base.metadata.create_all(engine)
 
 c1 = Customer(first_name='Toby',
               last_name='Miller',
@@ -21,4 +38,45 @@ c2 = Customer(first_name='Scott',
               address='424 Patterson Street',
               town='Beckinsdale'
               )
-c1, c2
+
+session.add(c1)
+session.add(c2)
+
+session.commit()
+
+c3 = Customer(
+    first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk"
+)
+
+c4 = Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk"
+)
+
+c5 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c6 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add_all([c3, c4, c5, c6])
+session.commit()
