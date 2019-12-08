f35823ffb8d7cd3384eac7de546f4ee0e3699734
Yesha016 <yum2@njit.edu>
2019-12-07 20:49:12 -0500

    Feature: Added delete() method to delete multiple records at once

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -231,3 +231,8 @@ session.query(Item).filter(
     Item.name.ilike("W%")
 ).update({"quantity": 60}, synchronize_session='fetch')
 session.commit()
+
+session.query(Item).filter(
+    Item.name.ilike("W%")
+).delete(synchronize_session='fetch')
+session.commit()
